ebf967d0813788213e32b95df5e74c7bcbd9d603
Roksolana Sainchuk <sanasamer920@gmail.com>
2025-10-16 21:24:28 +0200

    Add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    target: 'esnext',
-    outDir: 'build',
+    target: "esnext",
+    outDir: "build",
   },
   server: {
     port: 3000,
